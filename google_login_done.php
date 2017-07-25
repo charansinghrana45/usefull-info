b5d049c9f1ddcfada11c9842aaa87eb77eb458a9
charansinghrana45 <charansinghrana45@gmail.com>
2017-07-25 15:14:46 +0530

    Initial commit

---
+++ google_login_done.php
@@ -0,0 +1,47 @@
+<?php
+//defining
+require_once __DIR__ . '/vendor/autoload.php';
+const CLIENT_ID = '748294224449-s1aqkfmo4m02hs5hop4lpe6f857ssamu.apps.googleusercontent.com';
+const CLIENT_SECRET = 'thsAnmIlVyFOK7v4BAg01_9P';
+const REDIRECT_URI = 'http://localhost/rana/done.php';
+
+session_start();
+
+//initialization
+$client = new Google_Client();
+$client->setClientId(CLIENT_ID);
+$client->setClientSecret(CLIENT_SECRET);
+$client->setRedirectUri(REDIRECT_URI);
+$client->setScopes('email');
+
+$plus = new Google_Service_Plus($client);
+
+//Actual Process
+if(isset($_GET['code']))
+{
+	//verify received code
+	$client->authenticate($_GET['code']);
+	
+	//get access token
+	$_SESSION['access_token'] = $client->getAccessToken();
+	
+	header('location:'.filter_var('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'],FILTER_SANITIZE_URL));
+}
+
+if(isset($_SESSION['access_token']) && $_SESSION['access_token'])
+{
+	//set access token (access token will be used in every request)
+	$client->setAccessToken($_SESSION['access_token']);
+	
+	$me = $plus->people->get('me');
+	
+	echo $me['emails'][0]['value'];
+	echo "<br>";
+	echo $me['image']['url'];
+}
+else
+{
+	echo "<a href='".$authURL = $client->createAuthUrl()."'>googlelogin</a>";
+}
+
+?>
